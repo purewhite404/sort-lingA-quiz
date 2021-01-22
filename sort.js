7d52450645d3c3994e1a14abb07589555f05705a
pure_white_404 <jr2hao.23soda@gmail.com>
2021-01-23 01:04:05 +0900

    inputでそのままローカルストレージに保存するようにした
    保存ボタンをなくし、storageのkeyやidを問題文にした

--- sort.js
+++ sort.js
@@ -1,5 +1,5 @@
 // 問題文とページ数の組を作る
-const makePagenumSentence = (sentence) => {
+const separatePage = (sentence) => {
     let start = sentence.lastIndexOf('(p.'); // '(p.'を探して切り出すstart位置を決める
     let p = sentence.substr(start + 4, 3); // 数字部分からの3文字を切り出す
     let sentenceOnly = sentence.slice(0, start); // 問題文だけを取り出す
@@ -8,34 +8,39 @@ const makePagenumSentence = (sentence) => {
 };
 
 
-// 問題、自分の解答、セーブボタンを一つのlist itemにまとめる
-const addQA = (listItem, question, qNumber) => {
+// 問題、自分の解答を一つのlist itemにまとめる
+const addQA = (listItem, question) => {
     // 問題
     let questionNode = document.createElement('p');
     questionNode.appendChild(document.createTextNode(question));
-    questionNode.setAttribute('class', 'qa' + qNumber);
 
     // 自分の解答
     let answerElem = document.createElement('input');
     answerElem.setAttribute('size', 160)
-    answerElem.setAttribute('id', 'answer' + qNumber);
+    answerElem.setAttribute('id', 'answer' + question);
     let answerNode = document.createElement('p');
     answerNode.appendChild(answerElem);
-    answerNode.setAttribute('class', 'qa' + qNumber);
 
-    listItem.appendChild(questionNode)
-    listItem.appendChild(answerNode)
+    listItem.appendChild(questionNode);
+    listItem.appendChild(answerNode);
 };
 
 // 自分の解答をロードする
-const loadAnswer = (list) => {
-    for ( let qNumber in list ){
-        let key = 'answer' + qNumber;
+const loadAnswer = (qs) => {
+    for ( let qKey of qs ){
+        let key = 'answer' + qKey;
         let saveValue = localStorage.getItem(key);
-        if (saveValue != 'undefined'){
+        if (saveValue != 'undefined' && saveValue != null){
             document.getElementById(key).value = saveValue;
         }
     }
+};
+
+// 自分の解答をセーブする
+const saveAnswer = (qKey) => {
+    let key = 'answer' + qKey;
+    let inputBox = document.getElementById(key);
+    localStorage.setItem(key, inputBox.value);
 };
 
 // 一旦HTML内の li 要素をすべて取得
@@ -49,17 +54,15 @@ for ( let i in allList ){
     }
 }
 
-let dict = {};
-
 // 問題文(key)とページ(value)の連想配列を作成
-for ( let v of list ){
-    let [ s , p ] = makePagenumSentence(v);
-    s = '(p.' + p + ') ' + s;
-    dict[s] = p;
+let dict = {};
+for ( let i in list ){
+    let [ s , p ] = separatePage(list[i]);
+    dict['(p.' + p + ') ' + s] = p;
 }
 
 // 問題文をページ順にソート
-let questions=[];
+let questions = [];
 for(let q in dict)questions.push(q);
 questions.sort((a,b) => dict[a]-dict[b]);
 
@@ -71,33 +74,21 @@ let newListTable = document.createElement('ul');
 let newLi = [];
 for ( let i in questions ){
     newLi.push(document.createElement('li'));
-    addQA(newLi[newLi.length - 1], questions[i], i); // 問題、自分の解答、セーブボタンを一つのlist itemにまとめる
+    addQA(newLi[newLi.length - 1], questions[i]); // 問題、自分の解答を一つのlist itemにまとめる
 }
 for ( let v of newLi ){
     newListTable.appendChild(v);
 }
 let parentDiv = document.getElementsByTagName('font')[3]; // 3番目のfont tagが入れられるべきparentNode
 parentDiv.appendChild(newListTable);
-loadAnswer(list);
 
-// 保存ボタンを追加
-let button = document.createElement('button');
-button.appendChild(document.createTextNode('save'));
-button.setAttribute('onclick', 'saveAnswer(' + list.length + ')');
-parentDiv.appendChild(button); // parentNodeは同じくfont tag(3)
+// 答えが入力されている場合、localStorageからロードする
+loadAnswer(questions);
 
-// 保存するスクリプトを追加
-let scriptTag = document.createElement('script');
-let script = " const saveAnswer = (num) => { for (let i = 0; i < num; i++){ let key = 'answer' + i; let inputBox = document.getElementById(key); localStorage.setItem(key, inputBox.value); } }; ";
-let scriptNode = document.createTextNode(script);
-scriptTag.appendChild(scriptNode);
-let htmlTag = document.getElementsByTagName('html');
-htmlTag[0].appendChild(scriptTag);
-/* 可読性のあるscript
-const saveAnswer = (num) => {
-    for (let i = 0; i < num; i++){
-        let key = 'answer' + i;
-        let inputBox = document.getElementById(key);
-        localStorage.setItem(key, inputBox.value);
-    }
-};*/
+// inputされた都度localStorageに保存するようなイベントリスナーを設定する
+for ( let qKey of questions ){
+    let answerNode = document.getElementById('answer' + qKey);
+    answerNode.addEventListener('input',() => {
+        saveAnswer(qKey);
+    }, false);
+}
