3ed346d731e482f67ae7bdcf3c61ab9f3228c4af
pure_white_404 <jr2hao.23soda@gmail.com>
2021-01-22 15:51:11 +0900

    ページ数を先頭に表示

--- sort.js
+++ sort.js
@@ -1,8 +1,9 @@
 const makePagenumSentence = (sentence) => {
     let start = sentence.lastIndexOf('(p.'); // '(p.'を探して切り出すstart位置を決める
     let p = sentence.substr(start + 4, 3); // 数字部分からの3文字を切り出す
+    let sentenceOnly = sentence.slice(0, start); // 問題文だけを取り出す
     let pagenum = Number(p.replace(/[^0-9]/g, '')); // 数字だけ取り出す
-    return [sentence, pagenum];
+    return [sentenceOnly, pagenum];
 }
 
 let allList = [...document.getElementsByTagName('li')];
@@ -20,6 +21,7 @@ let dict = {};
 // 問題文(key)とページ(value)の連想配列を作成
 for ( let v of list ){
     let [ s , p ] = makePagenumSentence(v);
+    s = '(p.' + p + ') ' + s;
     dict[s] = p;
 }
 
@@ -32,14 +34,14 @@ questions.sort((a,b) => dict[a]-dict[b]);
 document.getElementsByTagName('ol')[0].remove();
 
 // 再描画
-let newOl = document.createElement('ol');
+let newListTable = document.createElement('ul');
 let newLi = [];
 for ( let v of questions ){
     newLi.push(document.createElement('li'));
     newLi[newLi.length - 1].appendChild(document.createTextNode(v));
 }
 for ( let v of newLi ){
-    newOl.appendChild(v);
+    newListTable.appendChild(v);
 }
 let parentDiv = document.getElementsByTagName('font')[3]; // 3番目のfont tagが入れられるべきparentNode
-parentDiv.insertBefore(newOl, null);
+parentDiv.insertBefore(newListTable, null);
